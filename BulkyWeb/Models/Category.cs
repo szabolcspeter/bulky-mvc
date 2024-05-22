2f3f599ee469de911cd0c1c3ab309a612f1d20aa
Szabolcs Péter <szabi.peter@gmail.com>
2024-05-22 10:43:14 +0200

    server side validation

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,9 +7,11 @@ namespace BulkyWeb.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The range of field Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
